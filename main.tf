7ff6deb04cce7d6b34b9c4fc4ee85b9fc7cbee90
neethugeorge <neethug@qburst.com>
2016-08-30 13:54:38 +0530

    Added variables to marathon module

--- main.tf
+++ main.tf
@@ -141,6 +141,8 @@ module "maraton-snapshot" {
   s3_config = "${var.marathon_snapshot_s3_config}"
 
   dcos_url = "${module.dcos.dcos_url}"
+  pre_tag = "${var.pre_tag}"
+  post_tag = "${var.post_tag}"
   dcos_url_token = "${module.dcos.dcos_acs_token}"
   dcos_config = "${var.marathon_snapshot_dcos_config}"
   dcos_env = "${var.marathon_snapshot_dcos_env}"
