a511e9374df14df61926af8af25464b44c3e4f5f
neethugeorge <neethug@qburst.com>
2016-08-30 09:35:49 +0530

    Making wrapper configurable

---
+++ configure.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#Include new modules in the module array.
+modules=( "docker-private-registry" "api-gateway" "elk" "marathon-snapshot" "ecr" )
+
+cat ./modules/dcos/dcos.tf > ./main.tf
+cat ./modules/dcos/dcos.dummy > ./terraform.dummy
+cat ./modules/dcos/dcos-variables.tf > ./variables.tf
+
+for module in "${modules[@]}"
+do
+   echo "Do you want to run $module ? yes/no"
+   read ANS
+   if [ ${ANS} == 'yes' ]
+   then
+   cat ./modules/$module/$module.tf >> ./main.tf
+   cat ./modules/$module/$module.dummy >> ./terraform.dummy
+   cat ./modules/$module/$module-variables.tf >> ./variables.tf
+   fi
+done
+
+terraform get -update
