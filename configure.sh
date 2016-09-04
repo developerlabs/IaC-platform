079c80a006e038766cbe19e424b4a621bbd3671a
KAMOL MAVLONOV <kamoljan@gmail.com>
2016-09-04 15:15:25 +0900

    Add coloring output prompt of configure.sh for better UI

--- configure.sh
+++ configure.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
-modules=( "docker-private-registry" "api-gateway" "elk" "marathon-snapshot" "ecr")
+modules=("docker-private-registry" "api-gateway" "elk" "marathon-snapshot" "ecr")
 
 cat ./modules/dcos/dcos.tf > ./main.tf
 cat ./modules/dcos/dcos.dummy > ./terraform.dummy
@@ -8,7 +10,7 @@ cat ./modules/dcos/dcos-variables.tf > ./variables.tf
 
 for module in "${modules[@]}"
 do
-   echo "Do you want to run $module ? yes/no"
+   echo -e "Do you want to run ${GREEN}$module${NC} ? yes/no"
    read ANS
    if [ ${ANS} == 'yes' ]
    then
