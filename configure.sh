3843c39d1d716922a18eac217c7fd4b4969ec161
Amal <amalv@qburst.com>
2016-09-07 10:40:19 +0530

    making tags configurable (#27)

--- configure.sh
+++ configure.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 GREEN='\033[0;32m'
+BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 modules=("docker-private-registry" "api-gateway" "elk" "marathon-snapshot" "ecr")
-
-cat ./modules/dcos/dcos.tf > ./main.tf
+VERSION=master
+echo -e "\nPlease specify the preferred version of the application (Leave empty for the master version).
+You can find the latest release version here.${GREEN}https://github.com/microservices-today/IaC-wrapper/releases.${NC}"
+read VERSION
+if [[ -z "$VERSION" ]]; then
+    VERSION=master
+fi
+cat ./modules/dcos/dcos.tf | sed "s/ref=master/ref="$VERSION/ > ./main.tf
 cat ./modules/dcos/dcos.dummy > ./terraform.dummy
 cat ./modules/dcos/dcos-variables.tf > ./variables.tf
 
@@ -14,7 +21,7 @@ do
    read ANS
    if [ ${ANS} == 'yes' ]
    then
-   cat ./modules/$module/$module.tf >> ./main.tf
+   cat ./modules/$module/$module.tf | sed "s/ref=master/ref="$VERSION/ >> ./main.tf
    cat ./modules/$module/$module.dummy >> ./terraform.dummy
    cat ./modules/$module/$module-variables.tf >> ./variables.tf
    fi
